b106e254c78795ece819750f484ea256cd488a53
Adrian Reyes <phipps11x19@gmail.com>
2025-11-07 15:56:41 -0400

    Commit 2

--- productos/listar.php
+++ productos/listar.php
@@ -12,7 +12,7 @@ $stmt->execute();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Productos</title>
+    <title>Gestión de Producto</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
